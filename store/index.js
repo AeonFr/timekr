78a264db11391e3e3f8d4fa01df275924191b85b
AeonFr <frankno.94@gmail.com>
2018-10-01 09:32:07 +0200

    convert to number the amount as well

--- store/index.js
+++ store/index.js
@@ -48,7 +48,7 @@ const createStore = () => {
 
         let assign = {};
         assign[project_id] = Object.assign(state.projects[project_id], {
-          time: Number(state.projects[project_id].time) + amount,
+          time: Number(state.projects[project_id].time) + Number(amount),
           updated_at: + new Date()
         });
 
