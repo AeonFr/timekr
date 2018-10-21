c42b861dfd70b3f7ed3a84271bc9c0c81ac9c671
AeonFr <frankno.94@gmail.com>
2018-10-21 14:50:34 +0200

    Better explained

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <h1 class="leading-loose">Welcome</h1>
-    <p>Create or select a project to continue.</p>
+    <h1 class="leading-loose">Welcome to Timekr</h1>
+    <p>This App can keep a score of the total time you spent in a project.</p>
+    <p>
+      Acording to the Pomodoro technique, 25 minutes is the optimal time concentration and
+      focus can be kept, so the Pomodoro timer incorporated in each project will count down from
+      25 minutes to zero, after that it will beep multiple times.
+    </p>
+    <p>
+      Time spent in each project will be commited either when the timer reaches zero, or by
+      using the "Insert time manually" function.
+    </p>
+    <p>
+      In the commit history of each project,
+      you can see when and how much time you added to that project.
+    </p>
+
+    <p>
+      <b>To start, add a new Project or select an existing one.</b>
+    </p>
   </div>
 </template>
