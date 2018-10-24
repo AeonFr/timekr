271e9876e3418c4445c8069df995a28769329096
AeonFr <frankno.94@gmail.com>
2018-10-24 12:15:02 +0200

    Copy cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,13 @@
       <p>This App can keep a score of the total time you spent in a project.</p>
       <p>
         Acording to the Pomodoro technique, 25 minutes is the optimal time concentration and
-        focus can be kept, so the Pomodoro timer incorporated in each project will count down from
-        25 minutes to zero, after that it will beep multiple times.
+        focus can be kept.
       </p>
       <p>
-        Time spent in each project will be commited either when the timer reaches zero, or by
-        using the "Insert time manually" function.
+        The Pomodoro timer incorporated in the Project Details View
+        will count backwards from 25 minutes to zero, and beep multiple times afterwards.
+        Your 25 minutes will be added automatically to your current project.
       </p>
-      <p>
-        In the commit history of each project,
-        you can see when and how much time you added to that project.
-      </p>
-
       <p>
         <b>To start, add a new Project or select an existing one.</b>
       </p>
