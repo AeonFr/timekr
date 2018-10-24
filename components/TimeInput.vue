0f9160c40a1e9c7534ea89a42451dfdefd797724
AeonFr <frankno.94@gmail.com>
2018-10-24 23:27:04 +0200

    Allow AND support negative numbers, behaving as expected

--- components/TimeInput.vue
+++ components/TimeInput.vue
@@ -5,7 +5,7 @@
       <input
         ref="hoursInput"
         :value="hours"
-        type="text"
+        type="number"
         pattern="-?\d+"
         name="hours"
         class="input text-lg font-mono"
@@ -17,7 +17,7 @@
       <input
         ref="minutesInput"
         :value="minutes"
-        type="text"
+        type="number"
         pattern="-?\d+"
         name="minutes"
         class="input text-lg font-mono"
@@ -37,17 +37,18 @@ export default {
   },
   computed: {
     hours(){
-      if (this.value == '-') return -0;
-      return Math.floor(Number(this.value)/60);
+      if (this.value >= 0)
+        return Math.floor(Number(this.value)/60);
+      else
+        return Math.ceil(Number(this.value)/60);
     },
     minutes(){
-      if (this.value == '-') return -0;
       return (Number(this.value) % 60);
     }
   },
   methods: {
     onInput($event){
-      let value = Number(this.$refs.hoursInput.value) * 60 + Number(this.$refs.minutesInput.value);
+      let value = (Number(this.$refs.hoursInput.value) * 60  || -0) + (Number(this.$refs.minutesInput.value) || -0);
       this.$emit('update:value', value);
     }
   }
