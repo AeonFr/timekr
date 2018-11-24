c9d5f6dab67e653784e48735535754e9765891e6
AeonFr <frankno.94@gmail.com>
2018-11-24 20:08:08 +0100

    initial commit

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,52 @@
+<template>
+  
+  <div>
+    <h1>Privacy Policy</h1>
+
+
+    <p>
+      This site uses both Cookies and <code>localStorage</code> to store your user data
+      (it should be safe unless you decide to clear your cache).</p>
+    <p>
+      <b>Unless you log in,</b>
+      any data you provide is saved on your current browser and device <b>only</b>.
+    </p>
+    <p>
+      By default, there's no server storing any data whatsoever,
+      so you're adviced to keep regular backups using the Export function.
+    </p>
+
+    <p>Keep reading if you want to create an account and you wonder how your data will be used...</p>
+
+    <h2>Privacy policy for authenticated users:</h2>
+
+    <p>
+      If you create an account, your data will be saved in the Firebase service, an online database service by Google.
+    </p>
+    <p>
+      Your email address will be shared with Firebase for the purpouse of authentication. Your data will be processed on Firebase's servers.
+    </p>
+
+    <p>
+      The use of analytics with your data (Firebase Analytics) <b>is not enabled for this app</b>.
+    </p>
+    <p>
+      The app registers the data you generate using the various input methods, like the project list and the pomodoro timer. Additionally, for security reasons, Firebase might keep a log of the date and time in wich you log-in.
+    </p>
+    <p>
+      Your data won't be shared with any other provider appart from Firebase. Timekr won't save more data than the necessary to keep the app running.
+    </p>
+    <p>
+      The owner of the app has access to user's data (your data included) through Firebase's platform.
+    </p>
+    <p>
+      User data (your data) might be queried only to solve bugs, or to check if you're still an active user. Never for marketing purpouses. After one or more years or inactivity, and if the database size increases, we can delete your account to reduce the digital footprint of this app.
+    </p>
+    <p>
+      User data will never be queried for other purpouses, not even to publish statistics about the ammount of users.
+    </p>
+    <p>
+      If you want to delete all data associated with your account, send an email to francanobr@outlook.es
+    </p>
+  </div>
+</template>
