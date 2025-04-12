1693b7334aa10481e0c58ef4aba3771758b70e79
fcano-ut <fcano@usertesting.com>
2025-04-12 13:32:10 +0200

    Update eslint config to fix some linting errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,21 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  }
 );
